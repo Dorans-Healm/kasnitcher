e5db894bc67ee1a4425ff8f1776453ed18ce32f8
Rafael Teixeira <rafael.txct@gmail.com>
2026-09-07 19:54:22 -0300

    [skip ci] refct/Separating color reading concerns

--- app/src/main/java/prism/application/port/ImageReader.java
+++ app/src/main/java/prism/application/port/ImageReader.java
@@ -1,14 +1,11 @@
 package prism.application.port;
 
+import prism.configuration.context.AppContext;
+import prism.infrastructure.filesystem.FileImageReader;
 import prism.utils.ColorUtils;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReadParam;
-import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class ImageReader {
 
@@ -16,49 +13,25 @@ public class ImageReader {
     private static final int COLOR_BUCKETS = 4096;
 
     public int[][] readColors(String path) throws IOException {
+        FileImageReader fileReader = (FileImageReader)
+                AppContext.instance().getClass(FileImageReader.class);
+
+        BufferedImage image =
+                fileReader.readSample(path, MAX_DIMENSION);
+
         int[] occurrences = new int[COLOR_BUCKETS];
 
-        try (ImageInputStream input = ImageIO.createImageInputStream(new File(path))) {
-            if (input == null) {
-                throw new IOException("Could not open image: " + path);
-            }
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
 
-            Iterator<javax.imageio.ImageReader> readers = ImageIO.getImageReaders(input);
+                int rgb = image.getRGB(x, y);
+                int bucket = ColorUtils.quantizeColor(rgb);
 
-            if (!readers.hasNext()) {
-                throw new IOException("Unsupported image format: " + path);
-            }
-
-            javax.imageio.ImageReader reader = readers.next();
-
-            try {
-                reader.setInput(input);
-
-                int width = reader.getWidth(0);
-                int height = reader.getHeight(0);
-
-                int sample = this.calculateSample(width, height);
-
-                ImageReadParam param = reader.getDefaultReadParam();
-                param.setSourceSubsampling(sample, sample, 0, 0);
-
-                BufferedImage image = reader.read(0, param);
-
-                for (int y = 0; y < image.getHeight(); y++) {
-                    for (int x = 0; x < image.getWidth(); x++) {
-                        int rgb = image.getRGB(x, y);
-                        int bucket = ColorUtils.quantizeColor(rgb);
-
-                        occurrences[bucket]++;
-                    }
-                }
-
-            } finally {
-                reader.dispose();
+                occurrences[bucket]++;
             }
         }
 
-        return this.toColorCountPairs(occurrences);
+        return toColorCountPairs(occurrences);
     }
 
     private int[][] toColorCountPairs(int[] occurrences) {
@@ -81,16 +54,5 @@ public class ImageReader {
         }
 
         return result;
-    }
-
-    private int calculateSample(int width, int height) {
-        int largestDimension = Math.max(width, height);
-
-        return Math.max(
-                1,
-                (int) Math.ceil(
-                        (double) largestDimension / MAX_DIMENSION
-                )
-        );
     }
 }
--- app/src/main/java/prism/infrastructure/filesystem/FileImageReader.java
+++ app/src/main/java/prism/infrastructure/filesystem/FileImageReader.java
@@ -1,4 +1,57 @@
 package prism.infrastructure.filesystem;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
 public class FileImageReader {
+
+    public BufferedImage readSample(String path, int maxDimension) throws IOException {
+        try (ImageInputStream input = ImageIO.createImageInputStream(new File(path))) {
+
+            if (input == null) {
+                throw new IOException("Could not open image: " + path);
+            }
+
+            Iterator<ImageReader> readers = ImageIO.getImageReaders(input);
+
+            if (!readers.hasNext()) {
+                throw new IOException("Unsupported image format: " + path);
+            }
+
+            ImageReader reader = readers.next();
+
+            try {
+                reader.setInput(input);
+
+                int width = reader.getWidth(0);
+                int height = reader.getHeight(0);
+
+                int sample = calculateSample(width, height, maxDimension);
+
+                ImageReadParam param = reader.getDefaultReadParam();
+                param.setSourceSubsampling(sample, sample, 0, 0);
+
+                return reader.read(0, param);
+            } finally {
+                reader.dispose();
+            }
+        }
+    }
+
+    private int calculateSample(int width, int height, int maxDimension) {
+        int largestDimension = Math.max(width, height);
+
+        return Math.max(
+                1,
+                (int) Math.ceil(
+                        (double) largestDimension / maxDimension
+                )
+        );
+    }
 }
