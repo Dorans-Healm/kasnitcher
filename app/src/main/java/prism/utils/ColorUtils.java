dad6b3ec93c34df490fd69947fe84c43322da83d
Rafael Teixeira <rafael.txct@gmail.com>
2026-09-07 21:37:28 -0300

    [skip ci] fix/Correcting bucket color parse

--- app/src/main/java/prism/utils/ColorUtils.java
+++ app/src/main/java/prism/utils/ColorUtils.java
@@ -2,6 +2,8 @@ package prism.utils;
 
 public class ColorUtils {
 
+    private static final int QUANTIZATION_BUCKET = 16 + 8;
+
     public static int quantizeColor(int rgb) {
         int r = (rgb >> 16) & 0xFF;
         int g = (rgb >> 8) & 0xFF;
@@ -15,18 +17,18 @@ public class ColorUtils {
     }
 
     public static String bucketToRgb(int bucket) {
-        int r = ((bucket >> 8) & 0xF) * 17;
-        int g = ((bucket >> 4) & 0xF) * 17;
-        int b = (bucket & 0xF) * 17;
+        int r = ((bucket >> 8) & 0xF) * QUANTIZATION_BUCKET;
+        int g = ((bucket >> 4) & 0xF) * QUANTIZATION_BUCKET;
+        int b = (bucket & 0xF) * QUANTIZATION_BUCKET;
 
         return "rgb(%d, %d, %d)".formatted(r, g, b);
     }
 
     public static String bucketToHex(int bucket) {
-        int r = ((bucket >> 8) & 0xF) * 17;
-        int g = ((bucket >> 4) & 0xF) * 17;
-        int b = (bucket & 0xF) * 16;
+        int r = ((bucket >> 8) & 0xF) * QUANTIZATION_BUCKET;
+        int g = ((bucket >> 4) & 0xF) * QUANTIZATION_BUCKET;
+        int b = (bucket & 0xF) * QUANTIZATION_BUCKET;
 
-        return String.format("#%02X%02X%02X", r, g, b);
+        return "#%02X%02X%02X".formatted(r, g, b);
     }
 }
